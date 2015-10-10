8ca8a846bcead36e17dce856642e9b1e89a6c768
Dang Pham <dangpham@mit.edu>
2015-10-10 12:54:46 -0400

    track.js and sound

---
+++ track.js
@@ -0,0 +1,31 @@
+var Track = function(tempo, sample, init_cb){
+	var that = Object.create(Track.prototype),
+		soundLoaded = false,
+		sample = sample,
+		sound = new p5.SoundFile(sample.path, trackLoaded);
+
+	var trackLoaded = function(cb){
+		var callback = cb ? cb : init_cb; 
+		soundLoaded = true;
+		callback();
+	};
+	that.play = function(){
+		sound.play();
+	};
+	that.getTempo = function(){
+		return tempo;
+	};
+
+	that.setTempo = function(new_tempo){
+		tempo = new_tempo;
+	};
+
+	that.changeSample = function(new_sample, callback){
+		sample = new_sample;
+		sound.setPath(sample.path, trackLoaded(callback));
+	};
+
+	Object.freeze(that);
+	return that;
+}
+
