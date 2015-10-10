e83a0c9982b598ee6b386918de31264b1a9fe9f8
Katie Bartel <kwbartel@mit.edu>
2015-10-10 14:44:14 -0400

    Planets play their sample when they pass through the top of their orbit

--- planet.js
+++ planet.js
@@ -3,25 +3,43 @@ var Planet = function(cX, cY, r, s, o) {
     var centerX = cX;
     var centerY = cY;
     var radius = r;
-    var speed = s;
+    var mainSpeed = s;
+    var speed = 0;
     var offset = o;
     var size = 80;
     var currentX = 0;
     var currentY = 0;
     var el = ellipse(centerX + radius, centerY + radius, size, size);
     var fillColor = color(0, 0, 0);
+    var curTrack;
+    var lastX = 0;
 
     that.update = function() {
         var milli = millis();
+        lastX = currentX;
         currentX = radius*cos(speed * milli + offset - Math.PI/2.0) + centerX;
         currentY = radius*sin(speed * milli + offset - Math.PI/2.0) + centerY;
         fill(fillColor);
         el = ellipse(currentX, currentY, size, size);
+
+        theta = (speed * milli + offset) % 2*Math.PI;
+
+        var xDiff = abs(currentX - centerX);
+        if ( lastX < centerX && currentX >= centerX && currentY < centerY
+         && curTrack) {
+            console.log(theta);
+            console.log('playing track');
+            curTrack.play();
+        }
     }
 
     that.contains = function(x, y) {
         return x < currentX + size && x > currentX - size
             && y < currentY + size && y > currentY - size;
+    }
+
+    that.start = function() {
+        speed = mainSpeed;
     }
 
     that.clicked = function() {
@@ -35,7 +53,10 @@ var Planet = function(cX, cY, r, s, o) {
     var privatemethod = function() {
 
     }
-
+    that.setTrack = function(track){
+        console.log("track set");
+        curTrack = track;
+    }
     Object.freeze(that);
     return that;
 }
---
+++ sketch.js
@@ -0,0 +1,56 @@
+var centerX;
+var centerY;
+var radius;
+var planet;
+
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+   createCanvas(windowWidth, windowHeight);
+   centerX = 350;
+   centerY = 400;
+    radius = 150;
+
+    orbits = [Orbit(centerX, centerY, 250), Orbit(centerX, centerY, 500)];
+    planets = [Planet(centerX, centerY, 125, .001, 0), Planet(centerX, centerY, 250, .002, Math.PI)];
+
+    track = Track(100, {path:'https://d34x6xks9kc6p2.cloudfront.net/85eac288-b659-4d8e-b2ba-67ad5def2458/85eac288-b659-4d8e-b2ba-67ad5def2458.mp3'},
+        function() {
+            planets[0].setTrack(track);
+            planets[0].start();
+        });
+    track2 = Track(100, {path:"https://d34x6xks9kc6p2.cloudfront.net/85eac288-b659-4d8e-b2ba-67ad5def2458/85eac288-b659-4d8e-b2ba-67ad5def2458.mp3"},
+        function() {
+            planets[1].setTrack(track2);
+            planets[1].start();
+        });
+
+
+
+}
+
+function draw() {
+  clear();
+  var speed = 0.002;
+
+  noFill();
+
+  // Draw planets and orbits
+   for (var i = 0; i < planets.length; i++) {
+        orbits[i].draw();
+        planets[i].update();
+    }
+}
+
+function mousePressed() {
+    for (var i = 0; i < planets.length; i++) {
+        if (planets[i].contains(mouseX, mouseY)) {
+                planets[i].clicked();
+        }
+    }
+}
+
+function mouseReleased() {
+    for (var i = 0; i < planets.length; i++) {
+        planets[i].released();
+    }
+}
